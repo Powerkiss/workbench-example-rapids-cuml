87e2349b99132f6226039375025132afdda09825
Powerkiss <jaanus.kiss@gmail.com>
2025-06-18 00:17:56 +0300

    Changed env w/ apt. Modified 2 files
    - Added apt packages: r-base, r-base-dev, libcurl4-openssl-dev,
    libssl-dev, libxml2-dev, libfontconfig1-dev, libharfbuzz-dev,
    libfribidi-dev
    - Modified files: apt.txt, postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -3,3 +3,12 @@
 # after all system packages and programming language specific package have been installed.
 #
 # Note: This file may be removed if you don't need to use it
+
+# Install IRkernel and register it with Jupyter
+Rscript -e "install.packages(c('repr', 'IRdisplay', 'IRkernel'), type = 'source', repos='http://cran.rstudio.com/')"
+Rscript -e "IRkernel::installspec(user = FALSE)"
+
+# Set default R library path for user installs (optional but good practice)
+mkdir -p ~/.R/library
+chmod -R 777 ~/.R/library
+echo "export R_LIBS_USER=~/.R/library" >> ~/.bashrc
